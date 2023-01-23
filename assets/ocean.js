9f980e8e0ea20488be29eff70c697e02ad09532b
jynnblyth <122335687+jynnblyth@users.noreply.github.com>
2023-01-23 12:41:14 +0800

    Update ocean.js

--- assets/ocean.js
+++ assets/ocean.js
@@ -41,7 +41,7 @@ AFRAME.registerComponent('ocean', {
     speedVariance: {default: 2},
 
     // Material.
-    color: {default: '#778786', type: 'color'},
+    color: {default: '#280137', type: 'color'},
     opacity: {default: 0.8}
   },
 
